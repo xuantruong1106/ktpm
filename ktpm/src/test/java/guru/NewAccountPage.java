d9ba5649593c84c91de8e0e6f29b4d06410e66f5
HoangDat47 <dat47707@gmail.com>
2024-10-30 17:00:48 +0700

    Change final

--- ktpm/src/test/java/guru/NewAccountPage.java
+++ ktpm/src/test/java/guru/NewAccountPage.java
@@ -4,12 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NewAccountPage {
-    private WebDriver driver;
-    private By customerIDField = By.xpath("//tbody/tr[2]/td[2]/input[1]");
-    private By accountTypeField = By.xpath("//tbody/tr[3]/td[2]/select[1]");
-    private By initialDepositField = By.xpath("//tbody/tr[4]/td[2]/input[1]");
-    private By getCustomerIDAlert = By.xpath("//label[@id='message14']");
-    private By initialDepositAlert = By.xpath("//label[@id='message19']");
+    private final WebDriver driver;
+    private final By customerIDField = By.xpath("//tbody/tr[2]/td[2]/input[1]");
+    private final By accountTypeField = By.xpath("//tbody/tr[3]/td[2]/select[1]");
+    private final By initialDepositField = By.xpath("//tbody/tr[4]/td[2]/input[1]");
+    private final By getCustomerIDAlert = By.xpath("//label[@id='message14']");
+    private final By initialDepositAlert = By.xpath("//label[@id='message19']");
 
     public NewAccountPage(WebDriver driver) {this.driver = driver;}
     public void setCustomerID(String customerID) {
